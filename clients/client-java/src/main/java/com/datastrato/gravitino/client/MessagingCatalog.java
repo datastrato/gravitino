a2ffcf4a1aa911b61a0e2f8f10aacf41ae728ffe
mchades <liminghuang@datastrato.com>
2024-03-28 14:23:19 +0800

    fix drop

---
+++ clients/client-java/src/main/java/com/datastrato/gravitino/client/MessagingCatalog.java
@@ -0,0 +1,12 @@
+/*
+ * Copyright 2024 Datastrato Pvt Ltd.
+ * This software is licensed under the Apache License version 2.
+ */
+package com.datastrato.gravitino.client;
+
+import com.datastrato.gravitino.messaging.TopicCatalog;
+
+public class MessagingCatalog extends BaseSchemaCatalog implements TopicCatalog {
+
+
+}
